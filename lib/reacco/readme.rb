b0b2f6e8ef028ecf7567290c82f83d5180595ced
Rico Sta. Cruz <rico@ambiescent.com>
2011-09-07 07:07:56 +0800

    Make sectioning default, and improve the help screen.

--- lib/reacco/readme.rb
+++ lib/reacco/readme.rb
@@ -47,7 +47,7 @@ module Reacco
         html = wrap_hgroup(html)
         html = move_pre(html)       if @options[:literate]
         html = brief_first_p(html)
-        html = section_wrap(html)   if @options[:sections]
+        html = section_wrap(html)
 
         html
       end
